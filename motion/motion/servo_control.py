a6b64a3c80ab053f850ebb85c18fdbb6f70c41d4
Datskalf <Datskalf@outlook.com>
2024-11-13 15:20:10 +0000

    Added some try-catches and changed topic address for servo control

--- motion/motion/servo_control.py
+++ motion/motion/servo_control.py
@@ -5,21 +5,33 @@ from interfaces.msg import ServoAngle
 from adafruit_servokit import ServoKit
 
 class ServoControl(Node):
+    kit: ServoKit | None = None
     def __init__(self):
         super().__init__("servo_control")
         self.get_logger().info(f"Servo control online!")
 
-        self.kit = ServoKit(channels=16)
+        try:
+            self.kit = ServoKit(channels=16)
+        except ValueError as ex:
+            self.get_logger().error(f"{ex}")
+            ...
 
 
         self.subscriber = self.create_subscription(
             ServoAngle,
-            "servo_angle",
+            "/subwoofer/servos/servo_update",
             self.servo_update,
             10
         )
 
+        self.get_logger().info(f"Servo controller online!")
+
+
+
     def servo_update(self, msg: ServoAngle):
+        if self.kit is None:
+            return
+        
         self.get_logger().info(f"Servo update: {msg.index} {msg.angle}")
         if msg.index not in [0,1,2,3,4,5,6,7,12,13,14,15]:
             return
@@ -34,6 +46,9 @@ class ServoControl(Node):
 def main(args=None):
     rclpy.init(args=args)
     node = ServoControl()
-    rclpy.spin(node)
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt as ex:
+        pass
     node.destroy_node()
     rclpy.shutdown()
