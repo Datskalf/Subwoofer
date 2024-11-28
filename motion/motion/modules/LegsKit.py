b07b2e756ca7501014904b3ef83187f47b4ae290
Datskalf <Datskalf@outlook.com>
2024-11-28 12:10:08 +0000

    Removed deprecated code

--- motion/motion/modules/LegsKit.py
+++ motion/motion/modules/LegsKit.py
@@ -21,47 +21,6 @@ class LegsKit():
     back_left: Leg = None
     back_right: Leg = None
 
-    def __init__(self, servo_addresses: list[list[int]], center_values: list[list[float]] | None = None) -> None:
-        """
-        TO BE DEPRECATED
-        Initialises a LegsKit object.
-        This initialiser will take a 2d array of I2C addresses, create each leg individually and store them is one LegsKit object.
-
-        :param list[list[int]] servo_addresses: A 2D array of I2C addresses.
-        :param list[list[float]] | None center_values: (Default None) Defines where the default values for the servos should be positioned. 
-        """
-
-        self.kit = ServoKit(channels=16)
-
-        self.front_left = Leg(
-            self.kit.servo[servo_addresses[0][0]],
-            self.kit.servo[servo_addresses[0][1]],
-            self.kit.servo[servo_addresses[0][2]]
-        )
-        self.front_right = Leg(
-            self.kit.servo[servo_addresses[1][0]],
-            self.kit.servo[servo_addresses[1][1]],
-            self.kit.servo[servo_addresses[1][2]],
-            True
-        )
-        self.back_left = Leg(
-            self.kit.servo[servo_addresses[2][0]],
-            self.kit.servo[servo_addresses[2][1]],
-            self.kit.servo[servo_addresses[2][2]]
-        )
-        self.back_right = Leg(
-            self.kit.servo[servo_addresses[3][0]],
-            self.kit.servo[servo_addresses[3][1]],
-            self.kit.servo[servo_addresses[3][2]],
-            True
-        )
-
-        if center_values is not None:
-            self.front_left._set_all_centers(center_values[0])
-            self.front_right._set_all_centers(center_values[1])
-            self.back_left._set_all_centers(center_values[2])
-            self.back_right._set_all_centers(center_values[3])
-
     def __init__(self) -> None:
         """
         Creates a new LegsKit object using the custom .modules.Servo implementation.
