d92c0ca31b5c74c82a1d1e3f276d4045a679ad3c
Datskalf <Datskalf@outlook.com>
2025-06-03 21:19:29 +0100

    Added angle capping test

--- subwoofer/test/test_servo_lib.py
+++ subwoofer/test/test_servo_lib.py
@@ -19,3 +19,18 @@ def test_dc_to_angle():
 
     for i in range(11):
         assert servo._duty_cycle_to_angle(servo.min_duty_cycle + i * dc_range/10) == servo.min_angle + i * ang_range/10
+
+def test_angle_capping():
+    from subwoofer.servo_pkg.Servo import Servo
+
+    servo = Servo(-1, False, True)
+
+    for ang in range(servo.min_angle - 10, servo.max_angle + 10, 5):
+        cap_ang = servo._cap_angle(ang)
+        assert cap_ang >= servo.min_angle and cap_ang <= servo.max_angle
+        if ang < servo.min_angle:
+            assert cap_ang == servo.min_angle
+        elif ang > servo.max_angle:
+            assert cap_ang == servo.max_angle
+        else:
+            assert cap_ang == ang
